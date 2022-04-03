7d85c876d89a55fe25bf6f312c4dadb31776a21e
Преподобный Ален <alienufo@inbox.ru>
2022-04-03 17:59:41 +0300

    Added docker.

---
+++ Dockerfile
@@ -0,0 +1,51 @@
+FROM debian AS builder
+
+LABEL project="apostol"
+
+ENV PROJECT_NAME 'apostol'
+
+ENV PG_VERSION '14'
+ENV PG_CLUSTER 'main'
+
+ENV TZ 'Europe/Moscow'
+
+ENV LANG 'ru_RU.UTF-8'
+ENV LC_ALL 'ru_RU.UTF-8'
+
+RUN set -eux; \
+    echo $TZ > /etc/timezone;  \
+    echo "en_US.UTF-8 UTF-8" > /etc/locale.gen; \
+    echo "ru_RU.UTF-8 UTF-8" >> /etc/locale.gen; \
+    apt-get update && apt-get install -y tzdata locales; \
+    rm /etc/localtime;  \
+    ln -snf /usr/share/zoneinfo/$TZ /etc/localtime;  \
+    dpkg-reconfigure -f noninteractive tzdata; \
+    dpkg-reconfigure -f noninteractive locales; \
+    update-locale; \
+    apt-get clean; \
+    apt-get update -y;
+
+RUN set -eux; \
+    apt-get install apt-utils bash build-essential cmake cmake-data g++ gcc libcurl4-openssl-dev libssl-dev make pkg-config sudo wget git htop mc lsb-release -y; \
+    sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'; \
+    wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -; \
+    apt-get update -y; \
+    apt-get install postgresql libpq-dev postgresql-server-dev-all -y;
+
+WORKDIR /opt/$PROJECT_NAME
+
+COPY . .
+
+RUN set -eux; \
+    cp docker/default.conf conf/default.conf; \
+    cp docker/default.json conf/sites/default.json; \
+    cmake -DCMAKE_BUILD_TYPE=Release . -B cmake-build-release; \
+    cd cmake-build-release; \
+    make install;
+
+RUN set -eux; \
+    pg_dropcluster $PG_VERSION $PG_CLUSTER;
+
+COPY ./docker/run.sh /opt/run.sh
+
+CMD ["bash", "/opt/run.sh"]
---
+++ docker/run.sh
@@ -0,0 +1,102 @@
+#!/bin/bash
+
+set -e
+
+if ! [[ $PROJECT_NAME ]]; then
+  PROJECT_NAME=apostol
+fi
+
+if ! [[ $LANG ]]; then
+  LANG=ru_RU.UTF-8
+fi
+
+if ! [[ $PG_VERSION ]]; then
+  PG_VERSION=14
+fi
+
+if ! [[ $PG_CLUSTER ]]; then
+  PG_CLUSTER=main
+fi
+
+if ! [[ $PG_DATA ]]; then
+  PG_DATA=/var/lib/postgresql/$PG_VERSION/$PG_CLUSTER
+fi
+
+pop_directory()
+{
+  popd >/dev/null
+}
+
+push_directory()
+{
+  local DIRECTORY="$1"
+  pushd "$DIRECTORY" >/dev/null
+}
+
+display_message()
+{
+  echo "$@"
+}
+
+display_error()
+{
+  >&2 echo "$@"
+}
+
+display_help()
+{
+  display_message "Usage: ./run.sh [OPTION]"
+  display_message "Script options:"
+  display_message "  --initdb                 Initialize database."
+  display_message "  --help                   Display usage, overriding script execution."
+  display_message ""
+}
+
+display_configuration()
+{
+  display_message "--------------------------------------------------------------------"
+  display_message "PROJECT_NAME    : $PROJECT_NAME"
+  display_message "LANG            : $LANG"
+  display_message "PG_VERSION      : $PG_VERSION"
+  display_message "PG_CLUSTER      : $PG_CLUSTER"
+  display_message "--------------------------------------------------------------------"
+}
+
+init_db()
+{
+  pg_createcluster --locale $LANG $PG_VERSION $PG_CLUSTER
+
+  push_directory /opt/$PROJECT_NAME/docker
+
+  cat postgresql.conf >> /etc/postgresql/$PG_VERSION/$PG_CLUSTER/postgresql.conf
+
+  chmod 640 pg_hba.conf
+  chown postgres:postgres pg_hba.conf
+  cp -p pg_hba.conf /etc/postgresql/$PG_VERSION/$PG_CLUSTER/pg_hba.conf
+
+  chmod 600 .pgpass
+  chown postgres:postgres .pgpass
+  cp -p .pgpass /var/lib/postgresql/.pgpass
+
+  pop_directory
+
+  pg_ctlcluster $PG_VERSION $PG_CLUSTER start
+
+  push_directory /opt/$PROJECT_NAME/db/sql
+  sudo -u postgres -H psql -d template1 -f make.psql
+  pop_directory
+
+  echo $(date '+%Y%m%d') > .initdb
+}
+
+display_configuration
+
+if ! [[ -d $PG_DATA ]]; then
+  init_db
+else
+  pg_ctlcluster $PG_VERSION $PG_CLUSTER start
+fi
+
+/usr/sbin/$PROJECT_NAME
+
+pg_ctlcluster $PG_VERSION $PG_CLUSTER stop
